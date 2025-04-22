430458ffa18c30dec42beb6ca1beb3739ab9dac5
Lukas Van Riel <lukas@Lukass-Mac-mini.local>
2025-04-22 09:53:32 -0400

    Added updated sampling area figure

--- 01_Scripts/SamplingRegion.R
+++ 01_Scripts/SamplingRegion.R
@@ -15,7 +15,7 @@ library(cowplot)
 
 data <- read_csv("/Users/lukas/Library/CloudStorage/OneDrive-UniversitedeMontreal/Data/Ch2/Final_Database/Combined.csv")
 
-betall <- st_read(here("zones-Raw", "BetAllegh", "data", "shapefile", "betualle.shp"),
+betall <- st_read("/Users/lukas/Library/CloudStorage/OneDrive-UniversitedeMontreal/Data/Ch2/Other/BetAllegh/data/shapefile/betualle.shp",
         layer = "betualle")
 plot(ba_sf)
 ba_sf <- st_as_sf(betall, coords = c("Longitude", "Latitude"), crs = 4326)
@@ -42,7 +42,7 @@ c <- ne_download(scale = 50, type = "countries", category = "cultural")
 test <- st_crop(roads, quebec)
 testc <- st_crop(test, bbox)
 plot(testc)
-CLASSI_ECO_QC.gdb
+
 # zones:
 zones <- st_layers(here("zones-Raw", "CLASSI_ECO_QC_GDB", "CLASSI_ECO_QC.gdb"))
 reg <- st_read(here("zones-Raw", "CLASSI_ECO_QC_GDB", "CLASSI_ECO_QC.gdb"),
@@ -108,25 +108,6 @@ inset_map <- ggplot() +
   theme_void() +
   labs(title = "")
 
-final_plot <- cowplot::ggdraw() +
-  cowplot::draw_plot(main_map) + 
-  cowplot::draw_plot(inset_map, x = 0.579, y = 0.5437, width = 0.4, height = 0.4) +
-  cowplot::draw_line(x = c(0.656, 0.656), y = c(0.544, 0.902), color = "black", size = 0.3) +
-  cowplot::draw_line(x = c(0.656, 0.902), y = c(0.544, 0.544), color = "black", size = 0.3)
-
-final_plot
-
-inset_map <- ggplot() +
-  #geom_sf(data = c_cropped, fill = "white", color = "black") +
-  geom_sf(data = states_cropped, fill = "white", color = "black", alpha=0) +
-  geom_sf(data = ba_sf, fill = "grey", color = "darkgrey") +
-  geom_sf(data = study_bbox, color = "red", alpha = 0.1, linewidth=0.5) +  # Highlight study region
-  coord_sf(xlim = c(-90, -58), ylim = c(30, 60)) +  # Zoom in closer
-  #geom_rect(aes(xmin = -125, xmax = -65, ymin = 20, ymax = 70), 
-  #          fill = NA, color = "black", linewidth = 0.1) +  # Box around inset
-  theme_void() +
-  labs(title = "") +
-  theme(panel.background = element_rect(fill = "white", color = "white"))  # White background
 inset_map
 
 combined_plot <- cowplot::plot_grid(main_map, inset_map, ncol = 2, rel_widths = c(3, 1))
@@ -146,6 +127,11 @@ cowplot::ggdraw() +
   cowplot::draw_plot(inset_map, x = 0.7, y = 0.4, width = 0.3, height = 0.9)
 
 #### gginset
+inset_cities <- data.frame(name = c("Montreal"), lat = c(45.5019), long = c(-73.5674)) %>% 
+  mutate(LONG = long, LAT = lat) %>% 
+  st_as_sf(coords = c("LONG", "LAT"), crs = 4326)
+inset_regions <- data.frame(name=c("ONTARIO", "QUEBEC", "USA"), lat=c(49.441, 50.391, 34.586), long=c(-86.582, -74.016, -87.103))
+
 main_map <- ggplot() +
   geom_sf(data = quebec_cropped, fill = "lightgray", color = "black", linewidth=0.3, alpha = 0.5) +
   geom_sf(data = r3c, fill = "orange", alpha = 0.3, show.legend = T) + 
@@ -159,22 +145,34 @@ main_map <- ggplot() +
   annotation_scale(location = "bl", pad_x = unit(0.55, "cm"), pad_y = unit(0.8, "cm"), style = "bar", width_hint=0.15) +
   annotation_north_arrow(location = "bl", style = north_arrow_fancy_orienteering, pad_x = unit(0.75, "cm"), pad_y = unit(1.4, "cm"),
                          height=unit(1, "cm"), width = unit(1, "cm")) +
-  labs(title = "Study Region",
+  labs(title = "",
        x = "", y = "") +
   theme_minimal() 
 
 inset_map <- ggplot() +
-  geom_sf(data = states_cropped, fill = "white", color = "black", alpha=0) +
-  geom_sf(data = ba_sf, fill = "grey", color = "darkgrey") +
-  geom_sf(data = study_bbox, color = "red", alpha = 0.1, linewidth=0.5) +  # Highlight study region
-  coord_sf(xlim = c(-90, -58), ylim = c(30, 60)) +  # Zoom in closer
+  #geom_sf(data = c_cropped, fill = "white", color = "black") +
+  geom_sf(data = states_cropped, fill = "white", color = "black", alpha=0, linewidth = 0.1) +
+  geom_sf(data = ba_sf, aes(fill="HR"), color="grey", show.legend = T) +
+  scale_fill_manual(name=" ", values=c("HR" = "grey"), labels = c("HR" = "Historical range")) + #\n
+  geom_sf(data = study_bbox, color = "red", alpha = 0.1, linewidth=0.5) +
+  geom_sf(data = inset_cities, color = "black", size = 1.2, shape = 15) +
+  geom_text(data = inset_regions, aes(x = long, y = lat, label =name), 
+            size = 2.5, hjust = c(0.45, 0.4, 0.1), vjust = c(-1.1, -1.4, 2), fontface = "bold") +
+  geom_text(data = inset_cities, aes(x = long, y = lat, label =name), 
+            size = 1.8, hjust = c(0.0), vjust = c(-1.1), fontface = "bold") +
+  coord_sf(xlim = c(-90, -58), ylim = c(30, 60)) +
+  #annotation_scale(location = "br", pad_x = unit(0.3, "cm"), pad_y = unit(0.2, "cm"), style = "bar") +  # Zoom in closer
   #geom_rect(aes(xmin = -125, xmax = -65, ymin = 20, ymax = 70), 
   #          fill = NA, color = "black", linewidth = 0.1) +  # Box around inset
+  labs(title = "") + 
   theme_void() +
-  labs(title = "") +
+  theme(legend.position = c(0.95, 0.02),
+        legend.justification = c(1,0),
+        legend.text = element_text(size=6),
+        legend.key.size = unit(0.4,"cm")) + # 0.3. with \n
   theme(panel.background = element_rect(fill = "white", color = "white")) 
 
-
+inset_map
 final_plot <- cowplot::ggdraw() +
   cowplot::draw_plot(main_map) + 
   cowplot::draw_plot(inset_map, x = 0.579, y = 0.5437, width = 0.4, height = 0.4) +
